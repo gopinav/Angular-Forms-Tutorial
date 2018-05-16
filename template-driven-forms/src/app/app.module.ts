f3f6d16b43ccee73c4bae1186c40f752e17388a0
Vishwas <gopinav5@gmail.com>
2018-05-16 21:21:55 +0530

    Import FormsModule

--- template-driven-forms/src/app/app.module.ts
+++ template-driven-forms/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule }   from '@angular/forms';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +8,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
