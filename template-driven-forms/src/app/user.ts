445ba66c581f5ff2d72d89e483aedbe0e36727fd
Vishwas <gopinav5@gmail.com>
2018-05-18 20:06:32 +0530

    Add User model

---
+++ template-driven-forms/src/app/user.ts
@@ -0,0 +1,10 @@
+export class User {
+    constructor(
+        public name: string,
+        public email: string,
+        public phone: number,
+        public topic: string,
+        public timePreference: string,
+        public subscribe: boolean
+    ) {}
+}
