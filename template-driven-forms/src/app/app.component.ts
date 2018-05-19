99c08eee4c3b819cff37d0b65c5147325b9c57b8
Vishwas <gopinav5@gmail.com>
2018-05-19 14:55:06 +0530

    Binding data to a model

--- template-driven-forms/src/app/app.component.ts
+++ template-driven-forms/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
   topics = ['Angular', 'React', 'Vue'];
+  userModel = new User('Rob', 'rob@test.com', 5556665566, 'Angular', 'morning', true);
 }
