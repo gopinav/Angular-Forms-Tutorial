2491fa1b06cd174411cd8f5ce023be89634c703a
Vishwas <gopinav5@gmail.com>
2018-08-07 15:23:13 +0530

    Multiple validations

--- reactive-forms/src/app/app.component.ts
+++ reactive-forms/src/app/app.component.ts
@@ -22,7 +22,7 @@ export class AppComponent {
   // });
 
   registrationForm = this.fb.group({
-    userName: ['', Validators.required],
+    userName: ['', [Validators.required, Validators.minLength(3)]],
     password: [''],
     confirmPassword: [''],
     address: this.fb.group({
